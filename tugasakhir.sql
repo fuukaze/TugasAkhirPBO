ca1a5945957c072584ff5d93f5893defdbd3d41f
M. Abdul Aziz <>
2021-06-02 14:30:01 +0700

    beberapa perubahan pada tampilan biodata

---
+++ tugasakhir.sql
@@ -0,0 +1,132 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 02, 2021 at 09:28 AM
+-- Server version: 10.4.17-MariaDB
+-- PHP Version: 8.0.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `tugasakhir`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `antrian`
+--
+
+CREATE TABLE `antrian` (
+  `id_antri` int(11) NOT NULL,
+  `nomor` int(11) NOT NULL,
+  `status_antri` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `biodata`
+--
+
+CREATE TABLE `biodata` (
+  `id_data` int(11) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `alamat` text NOT NULL,
+  `no_hp` varchar(20) NOT NULL,
+  `tipe_mobil` varchar(15) NOT NULL,
+  `nopol` varchar(9) NOT NULL,
+  `status_sewa` varchar(6) NOT NULL,
+  `km` int(11) NOT NULL,
+  `keluhan` text NOT NULL,
+  `id_montir` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `biodata`
+--
+
+INSERT INTO `biodata` (`id_data`, `nama`, `alamat`, `no_hp`, `tipe_mobil`, `nopol`, `status_sewa`, `km`, `keluhan`, `id_montir`) VALUES
+(1, 'sigit', 'kendal', '081243452974', 'avanza veloz', 'K1244RR', 'tidak', 2345, 'Suara mesinnya terdengar keras sekali', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `login`
+--
+
+CREATE TABLE `login` (
+  `id_login` int(11) NOT NULL,
+  `username` varchar(10) NOT NULL,
+  `password` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `login`
+--
+
+INSERT INTO `login` (`id_login`, `username`, `password`) VALUES
+(1, 'admin', 'admin');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `montir`
+--
+
+CREATE TABLE `montir` (
+  `id_montir` int(11) NOT NULL,
+  `nama_montir` varchar(30) NOT NULL,
+  `skill` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `montir`
+--
+
+INSERT INTO `montir` (`id_montir`, `nama_montir`, `skill`) VALUES
+(1, 'fuu', 'unlimited repair between 7AM - 12PM');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `antrian`
+--
+ALTER TABLE `antrian`
+  ADD PRIMARY KEY (`id_antri`);
+
+--
+-- Indexes for table `biodata`
+--
+ALTER TABLE `biodata`
+  ADD PRIMARY KEY (`id_data`),
+  ADD KEY `id_montir` (`id_montir`);
+
+--
+-- Indexes for table `login`
+--
+ALTER TABLE `login`
+  ADD PRIMARY KEY (`id_login`);
+
+--
+-- Indexes for table `montir`
+--
+ALTER TABLE `montir`
+  ADD PRIMARY KEY (`id_montir`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
