992b55ad94a03c6db39507ecc02a92c0f3e08f66
mabdu <mabdu@LAPTOP-JET2FR6E>
2021-06-04 18:33:14 +0700

    perubahan pada montir dan koneksi

--- src/Service/montir.java
+++ src/Service/montir.java
@@ -3,8 +3,8 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package biomontir;
-
+package Service;
+import Koneksi.*;
 import java.sql.Connection;
 import java.sql.SQLException;
 import javax.swing.table.DefaultTableModel;
